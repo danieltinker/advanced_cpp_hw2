50daed1a45373bcb7a647872ccf15847a28c7917
Daniel Baruch <danielb534@gmail.com>
2025-06-10 12:31:17 +0300

    comment

--- ArenaBattle/include/MyBattleInfo.h
+++ ArenaBattle/include/MyBattleInfo.h
@@ -4,13 +4,13 @@
 #include <cstddef>
 
 namespace arena {
-
+// This Struct is not allowed to contain pointers since it is shallowed copied to the tank algorithm.
 struct MyBattleInfo : public common::BattleInfo {
     std::size_t rows, cols;
     std::vector<std::vector<char>> grid;
     std::size_t selfX, selfY;      // tank’s own coord
     std::size_t shellsRemaining;   // <-- engine’s ammo count
-  
+
     MyBattleInfo(std::size_t r, std::size_t c)
       : rows(r)
       , cols(c)
